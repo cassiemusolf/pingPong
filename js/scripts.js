06065f8b078785fb09e1feffebc210a1acdc0f7e
Cassie Musolf <cassiemusolf@gmail.com>
2017-01-20 11:22:03 -0800

    add if statements to determine if ping or pong should be used

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,44 @@
 var pingPongGame = function(userNumber) {
 
 var emptyArray = [];
+var newArray = [];
+
 
   if (userNumber < 1) {
     alert ("Enter a number 1 or greater!");
   }
-    else if (userNumber >= 1) {
-    for (index=userNumber; index>0; index--) {
-      if (index % 3 === 0) {
-        emptyArray.push("ping");
-      }
+    if (userNumber >= 1) {
+      for (index=userNumber; index>0; index--) {
       emptyArray.push(index);
+  }
+  for (index=userNumber; index>0; index--) {
+    if (index % 3 === 0)
+    emptyArray.push("ping");
+  } if (index % 5 === 0) {
+      emptyArray.push("pong");
     }
-    } return emptyArray;
+    return emptyArray;
+  }
 }
 
-console.log("hi");
+
+
+
+
 $(document).ready(function() {
   $("form#pingPong").submit(function(pingPong) {
     event.preventDefault();
 
-    var userNumber = $("input#number").val();
+    var userNumber = parseInt($("input#number").val());
     var result = pingPongGame(userNumber);
 
     $("#results").text(result);
   });
 });
 
+// } else if (index % 5 === 0) {
+//  emptyArray.push("pong");
+// } else {
 
 // for (index = 10; index > 0; newNumber--);
 //   emptyArray.push();
