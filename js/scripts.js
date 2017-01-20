3b68d9521c206994b4fb92c7f2c1a1418520b7bc
Cassie Musolf <cassiemusolf@gmail.com>
2017-01-20 08:59:02 -0800

    add simplest spec that the program can run, receive user input and return it back to them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+var pingPongGame = function(userNumber) {
+
+  if (userNumber >= 1) {
+    return userNumber;
+  }
+}
+
+console.log("hi");
 $(document).ready(function() {
-  $("form#pingPong").submit(function(event) {
+  $("form#pingPong").submit(function(pingPong) {
     event.preventDefault();
 
     var userNumber = $("input#number").val();
-    var result = pingPong(userNumber);
+    var result = pingPongGame(userNumber);
+
     $("#results").text(result);
   });
 });
