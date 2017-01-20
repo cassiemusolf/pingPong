93a8c87c05a844236922358283e3937f7f23519b
Cassie Musolf <cassiemusolf@gmail.com>
2017-01-20 10:26:00 -0800

    add another spec to code. Add ping if number is divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 var pingPongGame = function(userNumber) {
 
 var emptyArray = [];
-var newNumber = userNumber.value;
 
-  if (userNumber >= 1) {
-   return userNumber + userNumber - 1;
-    }
-    else if (userNumber < 1) {
+  if (userNumber < 1) {
     alert ("Enter a number 1 or greater!");
   }
+    else if (userNumber >= 1) {
+    for (index=userNumber; index>0; index--) {
+      if (index % 3 === 0) {
+        emptyArray.push("ping");
+      }
+      emptyArray.push(index);
+    }
+    } return emptyArray;
 }
 
 console.log("hi");
